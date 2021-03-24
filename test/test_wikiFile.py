31686ce82f569b00d3f5b14e41c683f01fe07f80
Tim Holzheim <tim.holzheim@rwth-aachen.de>
2021-03-24 17:53:47 +0100

    Added test case for WikiFile

---
+++ test/test_wikiFile.py
@@ -0,0 +1,30 @@
+import unittest
+import jinja2
+import os
+
+from wikifile.wikiFile import WikiFile
+
+
+class TestWikiFile(unittest.TestCase):
+    """
+    Test the jinja template utils
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_template_name(self):
+        self.assertEqual(WikiFile.get_template_name("   TestTemplate"), "TestTemplate",
+                         "Whitespace at the beginning of template name was not removed")
+        self.assertEqual(WikiFile.get_template_name("TestTemplate   "), "TestTemplate",
+                         "Whitespace at the end of template name was not removed")
+        self.assertEqual(WikiFile.get_template_name("   TestTemplate\n"), "TestTemplate",
+                         "Whitespace at the beginning of template name was not removed")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
