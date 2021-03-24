f86598e8fa79274b66b363b375e5a87a2d325496
Tim Holzheim <tim.holzheim@rwth-aachen.de>
2021-03-24 23:13:33 +0100

    Incremented version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='wikirender',
-    version='0.0.2',
+    version='0.0.3',
     packages=['wikifile','templates','templates/macros'],
     classifiers=[
             'Programming Language :: Python',
