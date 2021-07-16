cdfb8f100778b0897be9378e9d7af956d2aef11a
Wolfgang Fahl <wf@bitplan.com>
2021-07-16 11:42:15 +0200

    sticks to 0.0.19 (unreleased)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='wikirender',
-    version='0.0.20',
+    version='0.0.19',
     packages=['wikifile','templates','templates/macros', 'wikifile/resources/metamodel'],
     classifiers=[
             'Programming Language :: Python',
